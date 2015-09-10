dfc107f5d2fc10d3771fafd773c67078e5e52f08
Brandon Peterman <brpeterman@gmail.com>
2015-09-10 18:01:37 -0500

    Allow arbitrary characters for swizzle functions.

--- lib/swizzleable.rb
+++ lib/swizzleable.rb
@@ -4,16 +4,27 @@
 #  #[]
 #  #[]=
 module Swizzleable
-  def swiz_positions
-    {'x' => 0, 'y' => 1, 'z' => 2, 'w' => 3}
+  def define_swiz_positions(positions = nil)
+    if positions == nil
+      @swizzle_positions = ['x', 'y', 'z', 'w']
+        .each_with_index
+        .each_with_object({}) do |c, i, hash|
+        hash[c] = i
+      end
+    else
+      @swizzle_positions = positions
+    end
   end
 
   def swizzle(pattern)
-    if pattern.chars.reject{|c| swiz_positions.keys[0..self.length-1].include? c}.length > 0
+    if !instance_variable_defined? :@swizzle_positions
+      define_swiz_positions
+    end
+    if pattern.chars.reject{|c| @swizzle_positions.keys[0..self.length-1].include? c}.length > 0
       raise ArgumentError.new "Unrecognized characters in swizzle pattern."
     end
     new_vector = self.dup
-    pattern.chars.each_with_index {|c, i| new_vector[i] = self[swiz_positions[c]]}
+    pattern.chars.each_with_index {|c, i| new_vector[i] = self[@swizzle_positions[c]]}
     new_vector
   end
 end
