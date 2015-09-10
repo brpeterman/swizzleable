99ce597da342854513c558e1c1af9f78096b4693
Brandon Peterman <brpeterman@gmail.com>
2015-09-10 17:11:35 -0500

    Added a couple examples.

--- examples/vector.rb
+++ examples/vector.rb
@@ -4,6 +4,7 @@ require 'swizzleable'
 class Vector < Array
   include Swizzleable
 
+  # Provides access to swizzle functions like #zyx and similar.
   def method_missing(method, *args, &block)
     if method.to_s.chars.reject{|c| swiz_positions.keys[0..self.length-1].include? c}.length == 0
       if method.to_s.length != self.length
@@ -17,4 +18,15 @@ class Vector < Array
       swizzle(method.to_s)
     end
   end
+end
+
+def test
+  v = Vector.new [1, 2, 3]
+  v.zyx # [3, 2, 1]
+  v.xxy # [1, 1, 2]
+  v.xy # NoMethodError
+  v.xyzw #NoMethodError
+
+  v = Vector.new [1, 2, 3, 4, 5]
+  v.xyzzy # ArgumentError
 end
